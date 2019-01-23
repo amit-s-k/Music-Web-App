bbdac313c811a1c7dd5a4d08bc3027a66856a54c
amit-s-k <amitsk2014@gmail.com>
2019-01-23 16:09:32 +0530

    Updated sql files

--- src/main/resources/H2PrefillUsingContextRefreshed.sql
+++ src/main/resources/H2PrefillUsingContextRefreshed.sql
@@ -1,4 +1,1 @@
 insert into music(track_id,track_comments,track_name) values (1,'charlie puth','done for me');
-insert into music(track_id,track_comments,track_name) values (2,'winona oak','that wasnt hope that was love');
-insert into music(track_id,track_comments,track_name) values (3,'coldplay','something just like this');
-insert into music(track_id,track_comments,track_name) values (4,'Imagine Dragons','Whatever It Takes');
